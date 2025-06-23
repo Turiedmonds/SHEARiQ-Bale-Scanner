31850c0382451308092ee1c48e83b5990a033251
Turiedmonds <turi-edmonds@hotmail.com>
2025-06-24 11:27:12 +1200

    Create jsqr.js

---
+++ jsqr.js
@@ -0,0 +1,3 @@
+// jsQR v1.4.0 placeholder
+// Unable to fetch actual file due to environment restrictions.
+// Please replace this file with the official jsQR.js (v1.4.0) from https://github.com/cozmo/jsQR
