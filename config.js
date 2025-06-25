f31a0c22cbed4ac119bbebf83f98c124214bdb7e
Turiedmonds <turi-edmonds@hotmail.com>
2025-06-25 18:25:28 +1200

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.CONFIG = {
-  oauthClientId: '220936743738-4gjocsgd6a7ihmailiqh7noigejt46bl.apps.googleusercontent.com',
-}
+  oauthClientId: '220936743738-4gjocsgd6a7ihmailiqh7noigejt46bl.apps.googleusercontent.com'
+};
